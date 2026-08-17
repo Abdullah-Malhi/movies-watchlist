146de5a4a87a38f2e44a7736cacaaf96a72bc8bd
Abdullah-Malhi <abdullahmalhi361@gmail.com>
2026-08-17 12:13:16 +0500

    Removed

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import movieRoutes from "./Routes/movieRoutes.js";
+
+const app = express();
+const PORT = 5001;
+
+app.use('/movies', movieRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+});
+// Middleware to parse JSON requests
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});

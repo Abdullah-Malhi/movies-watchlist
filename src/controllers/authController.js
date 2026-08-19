b906e15a45a7b506cd872b4c8385f796be5661a3
Abdullah-Malhi <abdullahmalhi361@gmail.com>
2026-08-19 13:43:27 +0500

    feat: add JWT secret and expiration to environment variables; implement token generation utility

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 import { prisma } from "@prisma/client";
 
 
-
+import generateToken from "../utils/generateToken.js";
 
 
 
@@ -22,7 +22,9 @@ const register = async (req, res) => {
     // Here you would typically create the user in the database
     const newUser = await prisma.user.create({ data: { name, email, password: hashedPassword } });      
 
-    res.status(201).json({ message: 'User registered successfully', user: newUser });
+    const token = generateToken(newUser.id, res);
+
+    res.status(201).json({ message: 'User registered successfully', user: newUser, token });
 }
 
 
@@ -31,7 +33,7 @@ const login = async (req, res) => {
     // Check if user exists
     const userExists = await prisma.user.findUnique({
         where: {
-            email: email,a=
+            email: email,
         },
     });
 
@@ -42,10 +44,22 @@ const login = async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, userExists.password);
     if (!isPasswordValid) {
         return res.status(400).json({ message: 'Invalid password' });
-    }   
-    res.status(201).json({ message: 'User logged in successfully', user: userExists });
+    }  
+
+    const token = generateToken(userExists.id, res);
+
+
+    res.status(201).json({ message: 'User logged in successfully', user: userExists, token });
 };
 
 
+const logout = (req, res) => {
+    res.cookie("token", "", {
+        httpOnly: true, 
+        expires: new Date(0)
+        });
+    res.status(200).json({ message: "Logged out successfully" });
+}
 
-export { register, login };
+
+export { register, login, logout };
